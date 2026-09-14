3c4176dc4d116af42bc907b6647dc745bf43db70
yydsxwh <yydsxwh@gmail.com>
2026-09-14 10:00:09 +0800

    feat: enable timetable crosshair styles

---
+++ main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import { applyNativeChrome } from './lib/native'
+import './index.css'
+import './crosshair.css'
+
+void applyNativeChrome()
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
